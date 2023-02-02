8fe86afdd9ea2b7f52e46dc4ec457b9d4c52c555
hhkim0729 <heehkim@student.42seoul.kr>
2023-02-02 17:02:13 +0900

    Add gaming functions

---
+++ main.js
@@ -0,0 +1,113 @@
+'use strict';
+
+const playBtn = document.querySelector('.play');
+const stopBtn = document.querySelector('.stop');
+const timer = document.querySelector('.timer');
+const counter = document.querySelector('.counter');
+const resultBox = document.querySelector('.result');
+const replayBtn = document.querySelector('.replay');
+const resultMessage = document.querySelector('.message');
+const bottom = document.querySelector('.bottom');
+const cats = document.querySelectorAll('.cat');
+const penguins = document.querySelectorAll('.penguin');
+
+const INITIAL_TIME = 10;
+
+addEventListener('load', () => {
+  const bottomRect = bottom.getBoundingClientRect();
+  const catRect = cats[0].getBoundingClientRect();
+  const maxTop = bottomRect.height - catRect.height;
+  const maxLeft = bottomRect.width - catRect.width;
+
+  let time = INITIAL_TIME;
+  let count = 10;
+  let interval = null;
+
+  function setTimer() {
+    timer.innerHTML = `0:${time--}`;
+    if (time < 0) {
+      finishGame('YOU LOST!');
+    }
+  }
+
+  function displayRandom(element) {
+    element.style.opacity = 1;
+    element.style.pointerEvents = 'all';
+    element.style.transform = `translate(${Math.random() * maxLeft}px,${
+      Math.random() * maxTop
+    }px)`;
+  }
+
+  function startGame() {
+    // Hide play button and show stop button
+    playBtn.classList.remove('visible');
+    stopBtn.classList.add('visible');
+
+    // Hide resultBox
+    resultBox.classList.remove('visible');
+
+    // Start timer
+    console.log(time);
+    setTimer();
+    interval = setInterval(setTimer, 1000);
+
+    // Set counter
+    counter.innerHTML = count;
+
+    // Display cats
+    cats.forEach(displayRandom);
+    penguins.forEach(displayRandom);
+  }
+
+  function finishGame(message) {
+    clearInterval(interval);
+    resultMessage.innerHTML = message;
+    resultBox.classList.add('visible');
+    time = INITIAL_TIME;
+    count = 10;
+  }
+
+  playBtn.addEventListener('click', () => {
+    startGame();
+  });
+
+  stopBtn.addEventListener('click', () => {
+    finishGame('Replay?');
+  });
+
+  replayBtn.addEventListener('click', () => {
+    startGame();
+  });
+
+  bottom.addEventListener('click', ({ target }) => {
+    if (resultBox.classList.contains('visible')) return;
+
+    const type = target.dataset.type;
+    if (type) {
+      if (type === 'penguin') {
+        finishGame('YOU LOST!');
+      } else if (type === 'cat') {
+        counter.innerHTML = --count;
+        target.style.opacity = 0;
+        target.style.pointerEvents = 'none';
+        if (count === 0) {
+          finishGame('YOU WON!');
+        }
+      }
+    }
+  });
+
+  bottom.addEventListener('mouseover', ({ target }) => {
+    const type = target.dataset.type;
+    if (type) {
+      target.style.transform += 'scale(1.1)';
+    }
+  });
+
+  bottom.addEventListener('mouseout', ({ target }) => {
+    const type = target.dataset.type;
+    if (type) {
+      target.style.transform = target.style.transform.replace('scale(1.1)', '');
+    }
+  });
+});
