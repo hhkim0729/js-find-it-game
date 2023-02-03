b7d30e880c5effb939cbfc46ac86772cb3a3cc48
hhkim0729 <heehkim@student.42seoul.kr>
2023-02-03 17:24:16 +0900

    Refactor

--- main.js
+++ main.js
@@ -2,15 +2,12 @@
 
 const description = document.querySelector('.description');
 const playBtn = document.querySelector('.play');
-const stopBtn = document.querySelector('.stop');
 const timer = document.querySelector('.timer');
 const counter = document.querySelector('.counter');
 const resultBox = document.querySelector('.result');
 const replayBtn = document.querySelector('.replay');
 const resultMessage = document.querySelector('.message');
 const bottom = document.querySelector('.bottom');
-const cats = document.querySelectorAll('.cat');
-const penguins = document.querySelectorAll('.penguin');
 
 const bgMusic = new Audio('sound/bg.mp3');
 const catSound = new Audio('sound/cat.mp3');
@@ -19,65 +16,99 @@ const alertSound = new Audio('sound/alert.wav');
 const winSound = new Audio('sound/win.mp3');
 
 const INITIAL_TIME = 10;
+const CAT_COUNT = 10;
+const PENGUIN_COUNT = 7;
+
+let started = false;
+let time = INITIAL_TIME;
+let count = CAT_COUNT;
+let interval = null;
 
 addEventListener('load', () => {
   const bottomRect = bottom.getBoundingClientRect();
-  const catRect = cats[0].getBoundingClientRect();
-  const maxTop = bottomRect.height - catRect.height;
-  const maxLeft = bottomRect.width - catRect.width;
   const descRect = description.getBoundingClientRect();
 
-  let time = INITIAL_TIME;
-  let count = 10;
-  let interval = null;
+  function playSound(sound) {
+    sound.currentTime = 0;
+    sound.play();
+  }
 
-  function setTimer() {
+  function clearItems() {
+    const cats = bottom.querySelectorAll('.cat');
+    const penguins = bottom.querySelectorAll('.penguin');
+
+    cats && cats.forEach((cat) => cat.remove());
+    penguins && penguins.forEach((penguin) => penguin.remove());
+  }
+
+  function showStopButton() {
+    const icon = document.querySelector('.fa-play');
+    if (icon) {
+      icon.classList.add('fa-stop');
+      icon.classList.remove('fa-play');
+    } else {
+      playBtn.style.visibility = 'visible';
+    }
+  }
+
+  function setTimerText() {
     timer.innerHTML = `0:${time--}`;
     if (time < 0) {
       finishGame('YOU LOST!', alertSound);
     }
   }
 
-  function displayRandom(element) {
-    element.style.opacity = 1;
-    element.style.pointerEvents = 'all';
-    element.style.transform = `translate(${Math.random() * maxLeft}px,${
-      Math.random() * maxTop
-    }px)`;
+  function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  function addItem(className, count, path) {
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = bottomRect.width;
+    const y2 = bottomRect.height;
+    for (let i = 0; i < count; i++) {
+      const item = document.createElement('img');
+      item.setAttribute('class', className);
+      item.setAttribute('alt', className);
+      item.setAttribute('src', path);
+      item.setAttribute('data-type', className);
+      const x = randomNumber(x1, x2 - item.width);
+      const y = randomNumber(y1, y2 - item.height);
+      item.style.transform = `translate(${x}px,${y}px)`;
+      bottom.appendChild(item);
+    }
   }
 
   function startGame() {
-    // Play music
-    bgMusic.currentTime = 0;
-    bgMusic.play();
+    started = true;
 
-    // Hide play button and show stop button
-    playBtn.classList.remove('visible');
-    stopBtn.classList.add('visible');
+    playSound(bgMusic);
+    clearItems();
+    showStopButton();
 
-    // Hide resultBox
-    resultBox.classList.remove('visible');
+    setTimerText();
+    interval = setInterval(setTimerText, 1000);
+    counter.innerText = count;
 
-    // Start timer
-    setTimer();
-    interval = setInterval(setTimer, 1000);
-
-    // Set counter
-    counter.innerHTML = count;
-
-    // Display cats
-    cats.forEach(displayRandom);
-    penguins.forEach(displayRandom);
+    addItem('cat', CAT_COUNT, 'img/cat.png');
+    addItem('penguin', PENGUIN_COUNT, 'img/penguin.png');
   }
 
   function finishGame(message, sound) {
+    started = false;
+
     clearInterval(interval);
+
     bgMusic.pause();
-    sound.play();
+    playSound(sound);
+
+    playBtn.style.visibility = 'hidden';
     resultMessage.innerHTML = message;
     resultBox.classList.add('visible');
+
     time = INITIAL_TIME;
-    count = 10;
+    count = CAT_COUNT;
   }
 
   description.addEventListener('mouseover', (e) => {
@@ -89,37 +120,38 @@ addEventListener('load', () => {
     }
   });
 
-  description.addEventListener('mouseout', ({ target }) => {
+  description.addEventListener('mouseout', () => {
     const visibleImg = description.querySelector('.help_img.visible');
     visibleImg && visibleImg.classList.remove('visible');
   });
 
   playBtn.addEventListener('click', () => {
-    startGame();
-  });
-
-  stopBtn.addEventListener('click', () => {
-    finishGame('Replay?', alertSound);
+    if (started) {
+      finishGame('Replay?', alertSound);
+    } else {
+      startGame();
+    }
   });
 
   replayBtn.addEventListener('click', () => {
     startGame();
+    resultBox.classList.remove('visible');
   });
 
   bottom.addEventListener('click', ({ target }) => {
-    if (resultBox.classList.contains('visible')) return;
+    if (!started) {
+      return;
+    }
 
     const type = target.dataset.type;
     if (type) {
       if (type === 'penguin') {
-        penguinSound.play();
+        playSound(penguinSound);
         finishGame('YOU LOST!', alertSound);
       } else if (type === 'cat') {
-        catSound.play();
-        catSound.currentTime = 0;
+        playSound(catSound);
+        target.remove();
         counter.innerHTML = --count;
-        target.style.opacity = 0;
-        target.style.pointerEvents = 'none';
         if (count === 0) {
           finishGame('YOU WON!', winSound);
         }
